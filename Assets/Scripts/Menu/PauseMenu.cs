d52a6efa8f2e4653ac71025a2bdff02dc609c78e
sadan1kirai <sadnisgod@gmail.com>
2025-07-25 10:53:58 +0300

    menuler arasi gecis ve pausefonk

---
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public CanvasGroup pauseCanvas;
+    public Text[] menuItems;         // Resume ve Main Menu yazıları
+    public Text arrowText;           // ">" işareti
+    public float offsetX = -40f;     // Ok sola hizalansın
+
+    private int selectedIndex = 0;
+    private bool isPaused = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isPaused)
+                Pause();
+            else
+                Resume();
+        }
+
+        if (isPaused)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                selectedIndex = (selectedIndex - 1 + menuItems.Length) % menuItems.Length;
+                UpdateArrowPosition();
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                selectedIndex = (selectedIndex + 1) % menuItems.Length;
+                UpdateArrowPosition();
+            }
+            else if (Input.GetKeyDown(KeyCode.Return))
+            {
+                ExecuteSelection();
+            }
+        }
+    }
+
+    private void UpdateArrowPosition()
+    {
+        Vector3 pos = menuItems[selectedIndex].rectTransform.position;
+        arrowText.rectTransform.position = new Vector3(
+            pos.x + offsetX,
+            pos.y,
+            pos.z
+        );
+    }
+
+    private void ExecuteSelection()
+    {
+        if (selectedIndex == 0)
+        {
+            Resume();
+        }
+        else if (selectedIndex == 1)
+        {
+            Time.timeScale = 1f;
+            SceneManager.LoadScene("StartMenu");
+        }
+    }
+
+    private void Pause()
+    {
+        isPaused = true;
+        Time.timeScale = 0f;
+        SetPausePanelVisible(true);
+        selectedIndex = 0;
+        UpdateArrowPosition();
+    }
+
+    private void Resume()
+    {
+        isPaused = false;
+        Time.timeScale = 1f;
+        SetPausePanelVisible(false);
+    }
+
+    private void SetPausePanelVisible(bool isVisible)
+    {
+        pauseCanvas.alpha = isVisible ? 1 : 0;
+        pauseCanvas.interactable = isVisible;
+        pauseCanvas.blocksRaycasts = isVisible;
+    }
+}
